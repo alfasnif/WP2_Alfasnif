cdb9ea2eb6b026b8625a5062c151c450c9cf6c1c
alfasnif <alfasnif@gmail.com>
2023-09-26 19:39:19 +0700

    tugas pertemuan 2

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya M Maulana Andi Alfachrizy
+            Saya tinggal di daerah Lenteng Agung
+            Olahraga yang saya sukai adalah Lari";
+        
+    }
+}
