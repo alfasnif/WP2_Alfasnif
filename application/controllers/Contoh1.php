547cac194e8c0352b0047567cdc90f9280620292
alfasnif <alfasnif@gmail.com>
2023-12-05 20:08:56 +0700

    Update Pertemuan 12

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+
+class Contoh1 extends CI_Controller {
+    public function index() {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Gracias Kevin
+            Saya tinggal di daerah Tanjungbarat
+            olahraga yang saya sukai adalah Bulutangkis";
+    }
+}
